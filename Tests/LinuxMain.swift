9d367be589e66e53dd38cabe6b0e1e99b90fec16
Yim Lee <yim_lee@apple.com>
2020-08-24 17:55:46 -0700

    CI setup

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,15 @@
-import XCTest
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAsyncDNSResolver open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftAsyncDNSResolver project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAsyncDNSResolver project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
-import AsyncDNSResolverTests
-
-var tests = [XCTestCaseEntry]()
-tests += AsyncDNSResolverTests.__allTests()
-
-XCTMain(tests)
+fatalError("Please use --enable-test-discovery to run the tests instead")
