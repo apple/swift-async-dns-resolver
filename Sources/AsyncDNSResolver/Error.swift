334aa2180dbdeb55d1e07dd9dcf94053e9bde63b
Yim Lee <yim_lee@apple.com>
2020-08-24 18:58:25 -0700

    Add header

--- Sources/AsyncDNSResolver/Error.swift
+++ Sources/AsyncDNSResolver/Error.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAsyncDNSResolver open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftAsyncDNSResolver project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAsyncDNSResolver project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import CAsyncDNSResolver
 
 // MARK: - Async DNS resolver errors
